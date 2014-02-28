83e59ecca4799fad357af268554ae90ff0162e26
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-02-28 18:25:42 +0530

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Marketnauts::Application.config.secret_key_base = 'b2b4bf58cde916603ee93ab80027cf7a5c86fc7499a300e1f897b6b5406c3f154b7ca58127928b10d0181ab3ab6ff1425a0b28dff71fc7535e0537c70807ca38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Marketnauts::Application.config.session_store :cookie_store, key: '_Marketnauts_session'
