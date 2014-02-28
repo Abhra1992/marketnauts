417d9ba350cb9a5b1c003b0275d8c3098841a60e
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-02-28 20:10:59 +0530

    Move secret token out of source control.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Marketnauts::Application.config.secret_key_base = 'b2b4bf58cde916603ee93ab80027cf7a5c86fc7499a300e1f897b6b5406c3f154b7ca58127928b10d0181ab3ab6ff1425a0b28dff71fc7535e0537c70807ca38'
+Marketnauts::Application.config.secret_key_base = ENV['SECRET_TOKEN']
